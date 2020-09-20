f998af6b9ad248bf763f114e95bc7d127baf5410
tiedan12345 <70628419+tiedan12345@users.noreply.github.com>
2020-09-20 13:30:39 +0800

    Add files via upload

---
+++ config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#define GAME_WIDTH 512
+#define GAME_HEIGHT 763
+#define GAME_TITLE "飞机大战 v1.0"
+#define GAME_RES_PATH "./plan.rcc"
+#define GAME_ICON ":/res/app.ico"
+
+#endif // CONFIG_H
