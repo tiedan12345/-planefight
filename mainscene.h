6dfabfdea57237551ce84131b2472878184b35ea
tiedan12345 <70628419+tiedan12345@users.noreply.github.com>
2020-09-20 13:31:14 +0800

    Add files via upload

---
+++ mainscene.h
@@ -0,0 +1,23 @@
+#ifndef MAINSCENE_H
+#define MAINSCENE_H
+
+#include <QWidget>
+
+namespace Ui {
+class MainScene;
+}
+
+class MainScene : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit MainScene(QWidget *parent = nullptr);
+    ~MainScene();
+    void initScene();
+
+private:
+    Ui::MainScene *ui;
+};
+
+#endif // MAINSCENE_H
